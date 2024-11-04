b9efbdfbcd80e49e945e94a69943309c2e0835c4
sinemgrbz <sinemcobanligurbuz@gmail.com>
2024-11-05 00:12:22 +0300

    added components structure

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,17 @@
-export default function Todo() {
+export default function Todo({text,completed,completetodo,onToggle,deletetodo}) {
     return (
         <>
-        
+            <li className={completed ? "completed" : ""}>
+                <div className="view">
+                    <input
+                        className="toggle" type="checkbox"
+                        checked={completetodo}
+                        onChange={onToggle}
+                    />
+                    <label>{text}</label>
+                    <button className="destroy" onClick={deletetodo}></button>
+                </div>
+            </li>
         </>
     )
 }
